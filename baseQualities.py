516a112b3af814e1dc6359489c483f71ddc2b44d
jimin001 <95269984+jimin001@users.noreply.github.com>
2022-02-15 12:58:18 -0800

    remove print statements

--- baseQualities.py
+++ baseQualities.py
@@ -1,19 +1,9 @@
 import pysam
 
-#bam = "/Users/jimin/PEPPER_clr/HG003.rerun_pacbio.clr.phased.haplotagged.chr20.bam"
-#samfile = pysam.AlignmentFile(bam, "rb")
-#print("count", samfile.count()) #count 295902
-
 class CommandLine():
     def __init__(self, inOptions = None):
         import argparse
-        self.parser = argparse.ArgumentParser(
-            description='Program prolog - a brief description of what this thing does',
-            epilog='Program epilog - some other stuff you feel compelled to say',
-            add_help=True,  # default is True
-            prefix_chars='-',
-            usage='%(prog)s [options] -option1[default] <input >output'
-        )
+        
         self.parser.add_argument('--inputBam', type=str, default=None, required=True, help='bam file to adjust quality scores')
         self.parser.add_argument('--outputBam', type=str, default=None, required=True, help='name of output bam file')
         self.parser.add_argument('--score', type=int, default=0, required=True, help='score to adjust all base quality scores to')
@@ -34,14 +24,13 @@ def replaceQualityScores(bam, outputfilename, score):
     :return: None
     """
     pysamfile = pysam.AlignmentFile(bam, "rb")
-    #print("header", pysamfile.header)
+   
     # read is AlignedSegment
     outfile = pysam.AlignmentFile(outputfilename, "wb", template=pysamfile)
     for read in pysamfile.fetch():
-        #header = read.header
+        
         length = read.query_length
-        #print(read.query_qualities)
-        #print(read.query_qualities)
+       
         read.query_qualities = [score]*length
         outfile.write(read)
     outfile.close()
